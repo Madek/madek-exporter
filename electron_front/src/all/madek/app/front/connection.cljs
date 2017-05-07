94333ae8ecfec9c502001ae4e180257cfaf24634
Thomas Schank <DrTom@schank.ch>
2017-05-07 19:16:52 +0200

    Setup "connection" form

--- electron_front/src/all/madek/app/front/connection.cljs
+++ electron_front/src/all/madek/app/front/connection.cljs
@@ -1,5 +1,8 @@
 (ns madek.app.front.connection
+  (:refer-clojure :exclude [str keyword])
   (:require
+    [madek.app.front.utils :refer [str keyword deep-merge presence]]
+
     [fipp.edn :refer [pprint]]
     [reagent.ratom :as ratom :refer [reaction]]
     [madek.app.front.state :as state]
@@ -12,8 +15,9 @@
 
 (defn connect []
   (js/console.log "CONNECT!")
-  (.send (.-ipcRenderer Electron) "madek:connect"
-         (-> @state/client-db :connection :form clj->js)))
+  ; TODO
+  ;(.send (.-ipcRenderer Electron) "madek:connect" (-> @state/client-db :connection :form clj->js))
+  )
 
 (defn update-form-data [fun]
   (swap! state/client-db
@@ -26,36 +30,48 @@
 
 (def form-data (reaction (-> @state/client-db :connection :form)))
 
+(def url-is-valid
+  (reaction
+    (boolean
+      (when-let [url (-> @form-data :url presence)]
+        (re-matches #"https?://[^/]+" url)))))
+
+(def form-is-valid url-is-valid)
+
 (defn form []
   [:div.form
-   [:div.form-group
-    [:label {:for "url"} "URL "]
-    [:input#url
-     {:type "text"
+   [:div.form-group {:class (if @url-is-valid "" "has-error")}
+    [:label {:for "url"} "Madek base URL "]
+    [:input.url.form-control
+     {:class (if @url-is-valid "" "has-error")
+      :type "url"
       :placeholder "URL of you madek instance"
       :value (:url @form-data)
       :on-change #(update-form-data-value
-                    :url (-> % .-target .-value))
-      }]]
+                    :url (-> % .-target .-value))}]]
    [:div.form-group
-    [:label {:for "login"} "Login / e-mail address / api-client name"]
-    [:input#url
+    [:label {:for "login"} "Login | e-mail address | api-client name"]
+    [:input.login.form-control
      {:type "text"
       :placeholder "Login"
       :value (:login @form-data)
       :on-change #(update-form-data-value
                     :login (-> % .-target .-value))}]]
    [:div.form-group
-    [:label {:for "password"} "Password / token"]
-    [:input#url
+    [:label {:for "password"} "Password | token"]
+    [:input.password.form-control
      {:type "password"
       :placeholder "Password"
       :value (:password @form-data)
       :on-change #(update-form-data-value
                     :password(-> % .-target .-value))
       }]]
-   [:button {:on-click #(connect)}
-    "Connect"]])
+   [:div.form-group.pull-right
+    [:button.btn.btn-primary
+     (merge {:on-click #(connect)}
+            (when (not @form-is-valid)
+              {:disabled "yes"}))
+     "Connect"]]])
 
 
 (defn page []
--- electron_front/src/all/madek/app/front/routes.cljs
+++ electron_front/src/all/madek/app/front/routes.cljs
@@ -9,10 +9,6 @@
     [secretary.core :as secretary :include-macros true :refer [defroute]]
     ))
 
-;(defroute main-page "/index.html" [] (reset! madek.app.front.state/current-page nil))
-
-;(defroute "/" [] (reset! madek.app.front.state/current-page nil))
-
 (defroute about-page "/about" []
   (reset! madek.app.front.state/current-page madek.app.front.about/page))
 
@@ -26,6 +22,5 @@
   (accountant/configure-navigation!
     {:nav-handler (fn [path] (secretary/dispatch! path))
      :path-exists? (fn [path] (secretary/locate-route path))})
-  ;(secretary/dispatch! "/about")
-  ;(accountant/dispatch-current!)
-  )
+  (secretary/dispatch! "/connection")
+  (accountant/dispatch-current!))
